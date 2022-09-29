ab2844172ed534fa93cd2481ffbad93776cf92b1
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-09-29 22:08:56 +0100

    sort direction

--- src/main/java/com/example/springbootblogrestapi/services/impl/PostServiceImpl.java
+++ src/main/java/com/example/springbootblogrestapi/services/impl/PostServiceImpl.java
@@ -33,8 +33,12 @@ public class PostServiceImpl implements PostService {
         return postResponse;
     }
 
-    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy){
-        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir){
+
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
 
         Page<Post> posts = postRepository.findAll(pageable);
 
