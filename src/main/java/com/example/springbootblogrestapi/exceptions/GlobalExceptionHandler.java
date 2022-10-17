840753971a1d0ed78b35b6115983602870474a76
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-10-17 17:48:00 +0100

    handle global exceptions

--- src/main/java/com/example/springbootblogrestapi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/springbootblogrestapi/exceptions/GlobalExceptionHandler.java
@@ -23,4 +23,11 @@ public class GlobalExceptionHandler {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
+
+    // global exceptions
+    @ExceptionHandler
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception, WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
