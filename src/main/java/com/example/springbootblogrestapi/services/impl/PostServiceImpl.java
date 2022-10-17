29a0408e3a9adbc5cde5246bb03bc9d97b167175
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-10-17 16:48:58 +0100

    using model mapper on post service

--- src/main/java/com/example/springbootblogrestapi/services/impl/PostServiceImpl.java
+++ src/main/java/com/example/springbootblogrestapi/services/impl/PostServiceImpl.java
@@ -87,21 +87,23 @@ public class PostServiceImpl implements PostService {
 
     // convert entity to DTO
     private PostDto mapToDto(Post post){
-        PostDto postDto = new PostDto();
-        postDto.setId(post.getId());
-        postDto.setTitle(post.getTitle());
-        postDto.setDescription(post.getDescription());
-        postDto.setContent(post.getContent());
+        PostDto postDto = mapper.map(post, PostDto.class);
+//        PostDto postDto = new PostDto();
+//        postDto.setId(post.getId());
+//        postDto.setTitle(post.getTitle());
+//        postDto.setDescription(post.getDescription());
+//        postDto.setContent(post.getContent());
         return postDto;
     }
 
     // convert DTO to entity
     private Post mapToEntity(PostDto postDto){
-        Post post = new Post();
-        post.setId(postDto.getId());
-        post.setTitle(postDto.getTitle());
-        post.setDescription(postDto.getDescription());
-        post.setContent(postDto.getContent());
+        Post post = mapper.map(postDto, Post.class);
+//        Post post = new Post();
+//        post.setId(postDto.getId());
+//        post.setTitle(postDto.getTitle());
+//        post.setDescription(postDto.getDescription());
+//        post.setContent(postDto.getContent());
         return post;
     }
 }
