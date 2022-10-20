5e87c58ad3f97395146ad6e8c7cba1d233eecf5b
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-10-20 23:04:52 +0100

    comment validation

--- src/main/java/com/example/springbootblogrestapi/payload/CommentDto.java
+++ src/main/java/com/example/springbootblogrestapi/payload/CommentDto.java
@@ -2,10 +2,22 @@ package com.example.springbootblogrestapi.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class CommentDto {
     private Long id;
+
+    @NotEmpty(message = "Name should not be null or empty")
     private String name;
+
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email
     private String email;
+
+    @NotEmpty
+    @Size(min = 10, message = "Comment body should not be less than 10 characters")
     private String body;
 }
