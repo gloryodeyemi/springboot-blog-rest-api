94803b97275a3959de0e6a8e3275295f187724bd
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-10-20 23:05:40 +0100

    valid annotation in comment controller

--- src/main/java/com/example/springbootblogrestapi/controllers/CommentController.java
+++ src/main/java/com/example/springbootblogrestapi/controllers/CommentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,7 +22,7 @@ public class CommentController {
     }
 
     @PostMapping("/posts/{postId}/comments")
-    public ResponseEntity<CommentDto> createComment(@PathVariable Long postId, @RequestBody CommentDto commentDto){
+    public ResponseEntity<CommentDto> createComment(@PathVariable Long postId, @Valid @RequestBody CommentDto commentDto){
         return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
     }
 
@@ -37,7 +38,7 @@ public class CommentController {
     }
 //
     @PutMapping("/posts/{postId}/comments/{id}")
-        public ResponseEntity<CommentDto> updateComment(@RequestBody CommentDto commentDto,
+        public ResponseEntity<CommentDto> updateComment(@Valid @RequestBody CommentDto commentDto,
                                                         @PathVariable(value = "id") Long commentId,
                                                         @PathVariable Long postId){
         CommentDto commentResponse = commentService.updateComment(postId, commentId, commentDto);
