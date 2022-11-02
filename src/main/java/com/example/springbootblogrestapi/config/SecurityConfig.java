06a06ef9e40708e52d9ec68ea0c55815e71e2082
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-11-02 23:27:20 +0100

    in memory

--- src/main/java/com/example/springbootblogrestapi/config/SecurityConfig.java
+++ src/main/java/com/example/springbootblogrestapi/config/SecurityConfig.java
@@ -1,9 +1,14 @@
 package com.example.springbootblogrestapi.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -14,9 +19,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails user = User.builder().username("glow").password("password").roles("USER").build();
+        UserDetails admin = User.builder().username("admin").password("admin").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
