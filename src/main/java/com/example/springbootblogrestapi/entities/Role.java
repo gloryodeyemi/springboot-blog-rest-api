1b66f8cebc1c86ade3cfce1084be1e42867f34f4
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-11-03 15:47:05 +0100

    role entity

---
+++ src/main/java/com/example/springbootblogrestapi/entities/Role.java
@@ -0,0 +1,19 @@
+package com.example.springbootblogrestapi.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 60)
+    private String name;
+}
