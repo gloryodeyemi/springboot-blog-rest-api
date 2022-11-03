94d9bbf993e203912a55b609f49dcd0dee359697
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-11-03 17:54:22 +0100

    user based security

--- src/main/java/com/example/springbootblogrestapi/config/SecurityConfig.java
+++ src/main/java/com/example/springbootblogrestapi/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.example.springbootblogrestapi.config;
 
+import com.example.springbootblogrestapi.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +21,9 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
 
     @Bean
     PasswordEncoder passwordEncoder(){
@@ -37,12 +43,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails user = User.builder().username("glow").password(
-                passwordEncoder().encode("password")).roles("USER").build();
-        UserDetails admin = User.builder().username("admin").password(
-                passwordEncoder().encode("admin")).roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(user, admin);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
+
+    //    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails user = User.builder().username("glow").password(
+//                passwordEncoder().encode("password")).roles("USER").build();
+//        UserDetails admin = User.builder().username("admin").password(
+//                passwordEncoder().encode("admin")).roles("ADMIN").build();
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
 }
